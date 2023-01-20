17d4e7b2c0b878e2fc9034b69d4abc02c893c044
mamadou42 <momocoly42@gmail.com>
2023-01-20 12:03:20 +0000

    initial commit 🎉

---
+++ components/Header.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image"
+import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, []);
+
+  return (
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <Image 
+          src="https://logos-marques.com/wp-content/uploads/2021/03/Netflix-logo.png" 
+          width={100} 
+          height={100} 
+          className="cursor-pointer object-contain" 
+          alt="Logo" 
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">News & Populars</li>
+          <li className="headerLink">My List</li>
+        </ul> 
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <Image src="https://rb.gy/g1pwyx" width={30} height={30} alt="account" className="cursor-pointer rounded" />
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      'upload.wikimedia.org',
+      'logos-marques.com',
+      'rb.gy',
+      'image.tmdb.org'
+    ],
+  },
   reactStrictMode: true,
 }
 
